16b92bec86164ab938676bbf8171961e5bb632b7
Joe <joe.reddy5@gmail.com>
2021-09-15 22:18:06 +0000

    Second line added to Project3.java - Second commit

--- Project3.java
+++ Project3.java
@@ -1,5 +1,6 @@
   class Project3 {
     public static void main(String[] args) {
       System.out.println("Java app has created using Docker for assignment hosted by Simplilearn");
+      System.out.println("Adding second line");
     }
   }
